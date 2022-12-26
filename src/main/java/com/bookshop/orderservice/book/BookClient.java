be4afd5b139993f7b6341a269d4be349ad951c28
Ahmed Khabkhab <Ahmed Khabkhab>
2022-12-26 12:29:59 +0100

    Add retries and timeout

--- src/main/java/com/bookshop/orderservice/book/BookClient.java
+++ src/main/java/com/bookshop/orderservice/book/BookClient.java
@@ -2,7 +2,11 @@ package com.bookshop.orderservice.book;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 public class BookClient {
@@ -19,7 +23,11 @@ public class BookClient {
                 .get()
                 .uri(BOOK_ROOT_API + isbn)
                 .retrieve()
-                .bodyToMono(Book.class);
+                .bodyToMono(Book.class)
+                .timeout(Duration.ofSeconds(3), Mono.empty())
+                .onErrorResume(WebClientResponseException.class, e -> Mono.empty())
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100L)))
+                .onErrorResume(Exception.class, e -> Mono.empty());
     }
 
 
