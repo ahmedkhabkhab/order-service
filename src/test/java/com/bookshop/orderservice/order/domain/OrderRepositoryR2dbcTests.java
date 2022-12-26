8a28883f1b28052071d53810fa2f5f943fc7736a
Ahmed Khabkhab <Ahmed Khabkhab>
2022-12-26 13:17:55 +0100

    Test data persistence with DataR2dbcTest and TestContainers

---
+++ src/test/java/com/bookshop/orderservice/order/domain/OrderRepositoryR2dbcTests.java
@@ -0,0 +1,52 @@
+package com.bookshop.orderservice.order.domain;
+
+import com.bookshop.orderservice.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import reactor.test.StepVerifier;
+
+@DataR2dbcTest
+@Import(DataConfig.class) // Enable auditing
+@Testcontainers // Activates automatic startup and cleanup of test containers
+public class OrderRepositoryR2dbcTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @DynamicPropertySource
+    public static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", OrderRepositoryR2dbcTests::r2dbcUrl);
+        registry.add("spring.r2dbc.username", postgresql::getUsername);
+        registry.add("spring.r2dbc.password", postgresql::getPassword);
+        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+    }
+
+    private static String r2dbcUrl() {
+        return String.format("r2dbc:postgresql://%s:%s/%s",
+                postgresql.getContainerIpAddress(),
+                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+                postgresql.getDatabaseName());
+    }
+
+    @Test
+    public void createRejectedOrder() {
+        var rejectedOrder = OrderService.buildRejectedOrder("123", 3);
+
+        StepVerifier
+                .create(orderRepository.save(rejectedOrder))
+                .expectNextMatches(order -> OrderStatus.REJECTED.equals(order.status()))
+                .verifyComplete();
+    }
+
+}
