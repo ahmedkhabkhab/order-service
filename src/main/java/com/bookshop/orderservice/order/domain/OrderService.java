f2cedce03beb0b40debfd305d092a4491a5b4cca
Ahmed Khabkhab <ahmed.khabkhab@gmail.com>
2023-01-21 12:20:46 +0100

    Add publishOrderAcceptedEvent and update application.xml

--- src/main/java/com/bookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/bookshop/orderservice/order/domain/OrderService.java
@@ -2,9 +2,11 @@ package com.bookshop.orderservice.order.domain;
 
 import com.bookshop.orderservice.book.Book;
 import com.bookshop.orderservice.book.BookClient;
+import com.bookshop.orderservice.order.event.OrderAcceptedMessage;
 import com.bookshop.orderservice.order.event.OrderDispatchedMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,10 +18,12 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final BookClient bookClient;
+    private final StreamBridge streamBridge;
 
-    public OrderService(OrderRepository orderRepository, BookClient bookClient) {
+    public OrderService(OrderRepository orderRepository, BookClient bookClient, StreamBridge streamBridge) {
         this.orderRepository = orderRepository;
         this.bookClient = bookClient;
+        this.streamBridge = streamBridge;
     }
 
     public Flux<Order> getAllOrders() {
@@ -50,6 +54,19 @@ public class OrderService {
                 book.price(),
                 quantity,
                 OrderStatus.ACCEPTED);
+    }
+
+    public void publishOrderAcceptedEvent(Order order) {
+        if (order.status() != OrderStatus.ACCEPTED) {
+            return;
+        }
+
+        var orderAcceptedMessage = new OrderAcceptedMessage(order.id());
+        log.info("Sending order accepted event for order with id: {}", order.id());
+        // acceptOrder binding is created automatically by Spring cloud stream at startup
+        var result = streamBridge.send("acceptOrder-out-0", orderAcceptedMessage);
+        log.info("Result of sending order accepted event for order with id {} is|: ", order.id(), result);
+
     }
 
     public Flux<Order> consumeOrderDispatchedEvent(Flux<OrderDispatchedMessage> flux) {
